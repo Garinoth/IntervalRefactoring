3636a060437172ce40795ce5cfefeb383ea78c57
Garinoth <cdavidrp@gmail.com>
2015-04-02 20:11:01 +0200

    Adapt factory to the new class structure

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -4,6 +4,12 @@ package intervals;
 public class IntervalFactory {
 
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
-		return new Interval(minimum, maximum, opening);
+		switch (opening) {
+		case UNOPENED:
+			return new UnopenedInterval(minimum, maximum);
+			
+		default:
+			return new Interval(minimum, maximum, opening);
+		}
 	}
 }
