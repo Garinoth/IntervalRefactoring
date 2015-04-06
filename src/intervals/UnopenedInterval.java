7d3506c4e7cccf9a83fd00e2c735438e59244b79
Garinoth <cdavidrp@gmail.com>
2015-04-06 23:10:43 +0200

    Copy conditional leg to subclass unopened

--- src/intervals/UnopenedInterval.java
+++ src/intervals/UnopenedInterval.java
@@ -15,4 +15,31 @@ public class UnopenedInterval extends Interval {
 	public boolean includes(double value) {
 		return getMinimum() <= value && value <= getMaximum();
 	}
+	
+	@Override
+	public boolean includes(Interval interval) {
+		boolean minimumIncluded = this.includes(interval.getMinimum());
+		boolean maximumIncluded = this.includes(interval.getMaximum());
+		switch (interval.getOpening()) {
+		case BOTH_OPENED:
+			return (minimumIncluded || getMinimum() == interval.getMinimum())
+					&& (maximumIncluded || getMaximum() == interval
+							.getMaximum());
+		case LEFT_OPENED:
+			return (minimumIncluded || getMinimum() == interval.getMinimum())
+					&& (maximumIncluded || getMaximum() == interval
+							.getMaximum());
+		case RIGHT_OPENED:
+			return (minimumIncluded || getMinimum() == interval.getMinimum())
+					&& (maximumIncluded || getMaximum() == interval
+							.getMaximum());
+		case UNOPENED:
+			return (minimumIncluded || getMinimum() == interval.getMinimum())
+					&& (maximumIncluded || getMaximum() == interval
+							.getMaximum());
+		default:
+			assert false;
+			return false;
+		}
+	}
 }
