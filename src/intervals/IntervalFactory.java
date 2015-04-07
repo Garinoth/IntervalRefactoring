4c71d1b5460ea80dded9e94e6c0817f0d0e0abe6
Garinoth <cdavidrp@gmail.com>
2015-04-07 21:28:21 +0200

    Refactor factory to use the new points

--- src/intervals/IntervalFactory.java
+++ src/intervals/IntervalFactory.java
@@ -5,14 +5,14 @@ public class IntervalFactory {
 
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
 		switch (opening) {
-		case UNOPENED:
-			return new UnopenedInterval(minimum, maximum);
 		case BOTH_OPENED:
-			return new BothOpenedInterval(minimum, maximum);
+			return new BothOpenedInterval(new OpenedPoint(minimum), new OpenedPoint(maximum));
 		case LEFT_OPENED:
-			return new LeftOpenedInterval(minimum, maximum);
+			return new LeftOpenedInterval(new OpenedPoint(minimum), new ClosedPoint(maximum));
 		case RIGHT_OPENED:
-			return new RightOpenedInterval(minimum, maximum);
+			return new RightOpenedInterval(new ClosedPoint(minimum), new OpenedPoint(maximum));
+		case UNOPENED:
+			return new UnopenedInterval(new ClosedPoint(minimum), new ClosedPoint(maximum));
 			
 		default:
 			throw new IllegalArgumentException("Invalid type code value");
