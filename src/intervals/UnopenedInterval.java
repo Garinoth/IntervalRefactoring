725d98bb8d8a0ffcabf13cdc6c60ade2eb1d0256
Garinoth <cdavidrp@gmail.com>
2015-04-08 02:52:07 +0200

    Fix duplicated code by using extract method (reverting the whole double dispatch thing)

--- src/intervals/UnopenedInterval.java
+++ src/intervals/UnopenedInterval.java
@@ -19,27 +19,7 @@ public class UnopenedInterval extends Interval {
 
 	@Override
 	public boolean includes(Interval interval) {
-		return interval.isIncluded(this);
-	}
-
-	@Override
-	public boolean isIncluded(BothOpenedInterval interval) {
-		return interval.getMinimum().lesserOrEqualsThan(this.getMinimum()) && interval.getMaximum().greaterOrEqualsThan(this.getMaximum());
-	}
-
-	@Override
-	public boolean isIncluded(LeftOpenedInterval interval) {
-		return interval.getMinimum().lesserOrEqualsThan(this.getMinimum()) && interval.getMaximum().greaterOrEqualsThan(this.getMaximum());
-	}
-
-	@Override
-	public boolean isIncluded(RightOpenedInterval interval) {
-		return interval.getMinimum().lesserOrEqualsThan(this.getMinimum()) && interval.getMaximum().greaterOrEqualsThan(this.getMaximum());
-	}
-
-	@Override
-	public boolean isIncluded(UnopenedInterval interval) {
-		return interval.getMinimum().lesserOrEqualsThan(this.getMinimum()) && interval.getMaximum().greaterOrEqualsThan(this.getMaximum());
+		return this.getMinimum().lesserOrEqualsThan(interval.getMinimum()) && this.getMaximum().greaterOrEqualsThan(interval.getMaximum());
 	}
 
 	@Override
